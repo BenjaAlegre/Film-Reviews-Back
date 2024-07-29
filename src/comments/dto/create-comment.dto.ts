018aef8ccca3dca518d3697c23e3f52bbca0fc41
GonzaLorenzoAdviters <capacitaciones06.06@adviters.com>
2024-07-29 15:06:20 -0300

    review and comment dto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,22 @@
-export class CreateCommentDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import { Review } from "../../reviews/entities/review.entity";
+import { User } from "../../users/entities/user.entity";
+
+export class CreateCommentDto {
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  review: Review;
+
+  @ApiProperty()
+  @IsString()
+  user: User;
+}
--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,26 @@
-export class CreateReviewDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+import { Film } from "../../films/entities/film.entity";
+import { User } from "../../users/entities/user.entity";
+
+export class CreateReviewDto {
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsNumber()
+  score: number;
+
+  @ApiProperty()
+  @IsString()
+  film: Film;
+
+  @ApiProperty()
+  @IsString()
+  user: User;
+}
