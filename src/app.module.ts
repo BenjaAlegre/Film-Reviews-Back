827369dd76d2297b4dd3acb2a0924215ce52a868
BenjaAlegre <capacitaciones06.02@adviters.com>
2024-07-29 11:20:36 -0300

    init2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilmsModule } from './films/films.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { RolesModule } from './roles/roles.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, FilmsModule, ReviewsModule, RolesModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
